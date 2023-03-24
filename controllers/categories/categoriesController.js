f7a702e2fec673b6a043954cd8601e42e9f514cb
Jonathan Ato Markin <jona10711@gmail.com>
2023-03-24 22:29:50 +0100

    Categories controller completed

---
+++ controllers/categories/categoriesController.js
@@ -0,0 +1,70 @@
+//Create category
+const createCategory = async (req, res) => {
+  try {
+    //
+    res.status(201).json({
+      responseCode: "00",
+      responseMessage: "Category created successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//Get all categories
+const getAllCategories = async (req, res) => {
+  try {
+    //
+    res.status(200).json({
+      responseCode: "00",
+      responseMessage: "Categories fetched successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//Get single category
+const getSingleCategory = async (req, res) => {
+  try {
+    //
+    res.status(200).json({
+      responseCode: "00",
+      responseMessage: "Category fetched successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//Update category
+const updateCategory = async (req, res) => {
+  try {
+    //
+    res.status(200).json({
+      responseCode: "00",
+      responseMessage: "Category updated successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+//Delete category
+const deleteCategory = async (req, res) => {
+  try {
+    //
+    res.status(200).json({
+      responseCode: "00",
+      responseMessage: "Category deleted successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//Export modules
+module.exports = {
+  createCategory,
+  getAllCategories,
+  getSingleCategory,
+
+  updateCategory,
+  deleteCategory,
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,71 +1,29 @@
 const express = require("express");
+//Import category controller
+const {
+  createCategory,
+  getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categories/categoryController");
 
 const categoryRouter = express.Router();
 
 //CATEGORY ROUTES
 //Create category endpoint
-categoryRouter.post("/create", async (req, res) => {
-  try {
-    //
-    res.status(201).json({
-      responseCode: "00",
-      responseMessage: "Category created successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+categoryRouter.post("/create", createCategory);
 
 //Get all categories endpoint
-categoryRouter.get("/", async (req, res) => {
-  try {
-    //
-    res.status(200).json({
-      responseCode: "00",
-      responseMessage: "Categories fetched successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+categoryRouter.get("/", getAllCategories);
 
 //Get single category endpoint
-categoryRouter.get("/:id", async (req, res) => {
-  try {
-    //
-    res.status(200).json({
-      responseCode: "00",
-      responseMessage: "Category fetched successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+categoryRouter.get("/:id", getSingleCategory);
 
 //Update category endpoint
-categoryRouter.put("/:id", async (req, res) => {
-  try {
-    //
-    res.status(200).json({
-      responseCode: "00",
-      responseMessage: "Category updated successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+categoryRouter.put("/:id", updateCategory);
 
 //Delete category endpoint
-categoryRouter.delete("/:id", async (req, res) => {
-  try {
-    //
-    res.status(200).json({
-      responseCode: "00",
-      responseMessage: "Category deleted successfully",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-});
+categoryRouter.delete("/:id", deleteCategory);
 
 module.exports = categoryRouter;
