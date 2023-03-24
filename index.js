78d1c3d81630d24b67f20ae4e232f0b0ef6c9125
Jonathan Ato Markin <jona10711@gmail.com>
2023-03-24 18:31:47 +0100

    Create different routes

--- index.js
+++ index.js
@@ -10,4 +10,97 @@ app.listen(PORT, () => {
 
 //middleware
 //routes
+
+//Register user endpoint
+app.post("/api/v1/users/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: user,
+      message: "User created successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Login user endpoint
+app.post("/api/v1/users/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if user exists
+
+    //check if password matches
+
+    //create token
+
+    //send token
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get all users endpoint
+app.get("/api/v1/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      data: users,
+      message: "All users fetched successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get single user endpoint
+app.get("/api/v1/users/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Update user endpoint
+app.put("/api/v1/users/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Delete user endpoint
+app.delete("/api/v1/users/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 //error handling
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    success: false,
+    error: err.message,
+  });
+});
+
+//Create post endpoint
+app.post("/api/v1/posts", async (req, res) => {
+  try {
+    //
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Get all posts endpoint
