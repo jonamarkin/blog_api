d6a8c384f7741c8f61765cce6650b0ba020a6913
Jonathan Ato Markin <jona10711@gmail.com>
2023-03-27 15:13:33 +0200

    Auth added to post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,18 +1,19 @@
 const express = require("express");
 //Import post controller
 const {
-  createPost,
-  getAllPosts,
-  getSinglePost,
-  updatePost,
-  deletePost,
+    createPost,
+    getAllPosts,
+    getSinglePost,
+    updatePost,
+    deletePost,
 } = require("../controllers/posts/postController");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 
 const postRouter = express.Router();
 
 //POST ROUTES
 //Create post endpoint
-postRouter.post("/create", createPost);
+postRouter.post("/create", isLoggedIn, createPost);
 
 //Get all posts endpoint
 postRouter.get("/", getAllPosts);
@@ -21,9 +22,9 @@ postRouter.get("/", getAllPosts);
 postRouter.get("/:id", getSinglePost);
 
 //Update post endpoint
-postRouter.put("/:id", updatePost);
+postRouter.put("/:id", isLoggedIn, updatePost);
 
 //Delete post endpoint
-postRouter.delete("/:id", deletePost);
+postRouter.delete("/:id", isLoggedIn, deletePost);
 
 module.exports = postRouter;
